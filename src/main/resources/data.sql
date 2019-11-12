ea8de675bbb616509ccc7173988c38017a1d8215
enhakkore <ahn.ephraim@gmail.com>
2019-11-12 17:39:49 +0900

    Update & Fix typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-inset into Reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
+insert into Reader (username, password, fullname) values ('craig', '{noop}password', 'Craig Walls');
