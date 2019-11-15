0a7d02d211ae3c18dd1499d607ce52cf21437e9a
enhakkore <ahn.ephraim@gmail.com>
2019-11-15 21:05:30 +0900

    Add test of authenticated

--- src/test/java/readinglist/MockMvcWebSecurityTests.java
+++ src/test/java/readinglist/MockMvcWebSecurityTests.java
@@ -9,12 +9,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,5 +44,21 @@ public class MockMvcWebSecurityTests {
         mockMvc.perform(get("/"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", "http://localhost/login"));
+    }
+
+    @Test
+    @WithUserDetails("craig")
+    public void homePage_authenticatedUser() throws Exception {
+        Reader expectedReader = new Reader();
+        expectedReader.setUsername("craig");
+        expectedReader.setPassword("{noop}password");
+        expectedReader.setFullname("Craig Walls");
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attribute("reader", samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)))
+                .andExpect(model().attribute("amazonId", "yourId"));
     }
 }
